967aa0615a8a2ec5e06c1ffd0a37a0f0a4a4a6d6
Burlesco70 <mario.nardi.70@gmail.com>
2021-10-27 20:17:18 +0200

    splashscreen

--- blood_donor_lez2/packages/donor/webpages/index.py
+++ blood_donor_lez2/packages/donor/webpages/index.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
             
 class GnrCustomWebPage(object):
-    py_requires = 'plainindex'
+    #py_requires = 'plainindex'
+    py_requires = 'frameindex'
+    index_url = 'html_pages/splashscreen.html'
+    def loginSubititlePane(self,pane):#here you can define the sub title as you required a 
+        pane.div('Blood Donor App',
+                    text_align='center',
+                    font_size='.9em',
+                    font_style='italic',color='#AAAAAA')   
     
