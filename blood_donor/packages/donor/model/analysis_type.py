062386880b36cafc78912102b5550e24417dc428
Davide Paci <davide.paci@softwell.it>
2021-11-18 08:32:37 +0100

    Aggiornata tabella gerarchica con modifiche fatte durante lez.4

--- blood_donor/packages/donor/model/analysis_type.py
+++ blood_donor/packages/donor/model/analysis_type.py
@@ -4,6 +4,8 @@
 class Table(object):
     def config_db(self, pkg):
         tbl =  pkg.table('analysis_type',pkey='id',name_long='!![en]Analysis type',
-                            name_plural='!![en]Analysis types',caption_field='description')
-        self.sysFields(tbl, hierarchical='description')
+                            name_plural='!![en]Analysis types',caption_field='name')
+        self.sysFields(tbl, hierarchical='name')
+        
+        tbl.column('name',name_long='!![en]Name')
         tbl.column('description',name_long='!![en]Description')
